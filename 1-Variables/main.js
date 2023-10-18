8f12f641e56bf748f57543b0a7e2577cf6938e96
javier gimenez <javierzlafe@gmail.com>
2023-10-18 12:22:06 -0300

    clase de ciclos e iteraciones

---
+++ 1-Variables/main.js
@@ -0,0 +1,79 @@
+
+                                                        //VARIABLES
+
+
+/* Es un espacio reservado en la memoria, que contiene un tipo de dato.
+sintaxis para declarar una variable */
+
+// tipo, nombre = dato
+
+//var mate = "yerba"  //no es recomendado, cada vez que uso VAR se muere un gatito
+
+//let vaso = "agua"  //usamos esta
+
+//const termo = "agua caliente"  //tipo de dato CONSTANTE, no cambia
+
+
+//string, es un tipo de dato que pertenece a una cadena de caracteres (osea letras,simbolos,etc)
+//number, es un tipo de dato que pertenece a numeros
+//bool, es un tipo de dato que solo representa VERDADERO O FALSO.
+
+
+
+//EJEMPLOS DE OPERACIONES
+
+    /* let  numeroA = 1
+    let   numeroB = 2
+    const numeroC = 3 */
+    //Suma  de dos números (1 + 2 = 3)
+   // let resultadoSuma  = numeroA + numeroB;
+
+
+    //Resta de dos números (2 - 1 = 1)
+   // let resultadoResta = numeroB - numeroA;
+
+
+    //Producto de dos números (2 * 3 = 6)
+   // let resultadoProducto = numeroB * numeroC;
+
+
+   /*  let nombre = 5
+    let apellido = 15 */
+
+
+    //let nombreCompleto = nombre + apellido   //concatenar es la sumatoria de diferentes datos.
+
+
+
+    //alert(nombreCompleto)   //dispara un cartel de alerta en mi pantalla
+
+
+
+
+
+
+                    //CONSOLE,PROMPT, ALERT
+
+
+
+
+//alert(15)   //dispara un cartel de alerta en mi pantalla
+
+
+
+//let nombreUsuario =  prompt("ingresa por favor tu nombre")
+
+
+//alert ( "bienvenido " + nombreUsuario)
+
+
+
+
+
+
+
+    //TIPOS DE MENSAJE POR CONSOLA
+
+console.log("mensaje normal")
+console.warn("mensaje de alerta")
+console.error("exploto todo")
---
+++ 2-Ciclos e iteraciones/main.js
@@ -0,0 +1,100 @@
+
+
+
+
+/* let nombre = prompt("ingresa tu nombre") */
+
+//condicion,true,falso
+
+/* 
+if(nombre === "carlos"){
+    alert("bienvenido carlos")
+}else{
+    alert("que mira bobo!")
+}
+ */
+
+
+/* let gaston = falso
+
+if (gaston){
+    alert("gaston esta en la clase")
+} */
+
+
+/* 
+let nombre = prompt("decime tu nombre").toLowerCase()
+
+
+
+
+if(nombre == "nico"){
+    alert("bienvenido querido " + nombre)
+}else if(nombre == "carlos"){
+    alert("no sos nico, pero sos "+ nombre + " asi que bienvenido")
+}else if (nombre=="tomas"){
+    alert(`hola ${nombre}, pongo la pava?`)
+}else{
+    alert("no sos mi amigo ")
+}
+ */
+
+
+
+//toUpperCase convierte TODO A MAYUSCULA
+//toLowerCase CONVIERTE TODO A minuscula
+
+
+
+//< > <= >= !=
+
+
+
+
+
+//                                             (AND, OR , NOT)  &&, || !
+
+
+
+/* let numA = 5
+let numB = 10
+
+
+if(numA<numB != numA<3){
+    alert("todo ok")
+} */
+
+
+
+
+
+/* let nombreIngresado   = prompt("Ingresar nombre");
+
+if(  (nombreIngresado !="")&&((nombreIngresado == "EMA") || (nombreIngresado =="ema")    )){
+    alert("Hola Ema"); 
+}else{
+    alert("Error: Ingresar nombre valido");
+}
+ */
+
+
+
+/* 
+producto, alguien que compre, precio base, si me conviene o no vender, avisar por kmensaje que paso*/
+
+
+alert("vendo algo, escucho ofertas")
+let ofertaBase = 2000
+let ofetrtaUsuario = prompt("ingresa tu oferta")
+if (ofetrtaUsuario <1000){
+    alert("no papa, sos un rata")
+}else if(ofetrtaUsuario >=1000 && ofetrtaUsuario <=1999){
+    alert("dale flaco, estirate un poco mas")
+}else if (ofetrtaUsuario >= ofertaBase){
+    alert("te lo vendo gracias por la compra")
+}else{
+    alert("tu oferta no se entendio")
+}
+
+
+
